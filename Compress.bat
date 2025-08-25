d009d52accb94f43d06fd9381345b956267d4eb6
Tomáš Raček <tom@krab1k.net>
2025-08-25 07:47:51 +0200

    Remove obsolete references

--- Compress.bat
+++ Compress.bat
@@ -1,4 +1,2 @@
-del TunnelsBeta.zip
 del TunnelsServer.zip
-"C:\Program Files\7-Zip\7z" a -tzip TunnelsBeta.zip TunnelsBeta\
 "C:\Program Files\7-Zip\7z" a -tzip TunnelsServer.zip TunnelsServer\
--- CompressMole2.bat
+++ CompressMole2.bat
@@ -1,6 +1,4 @@
 del Mole2_cmd.zip
 del Mole2_pymol.zip
-del Mole2_ui.zip
 "C:\Program Files\7-Zip\7z" a -tzip Mole2_cmd.zip Mole2_cmd\
 "C:\Program Files\7-Zip\7z" a -tzip Mole2_pymol.zip Mole2_pymol\
-"C:\Program Files\7-Zip\7z" a -tzip Mole2_ui.zip Mole2_ui\
